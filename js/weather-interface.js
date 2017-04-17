116b0d8658843deb9745a6c889028262e231d54a
Gloria Friesen and Mark Fisher <gloria@>
2017-04-17 16:33:15 -0700

    restore functionality by backtracking to before the asynchronicity handling

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,20 +1,11 @@
 var apiKey = require('./../.env').apiKey;
+var Weather = require('./../js/weather.js').weatherModule;
 
-$(document).ready(function(){
-  $("#weather-location").click(function(){
-    var city = $("#location").val();
-    $("#location").val("");
-    $(".showWeather").text("The city you have chosen is " + city + ".");
-
-    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey)
-      .then(function(response){
-        // console.log(response.responseJSON.message);
-        // // console.log(error.responseJSON.message);
-        $(".showWeather").text("The humidity in " + city + " is " + response.main.humidity + "%");
-      })
-
-      .fail(function(error) {
-        $('.showWeather').text(error.responseJSON.message);
-      });
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather();
   });
 });
