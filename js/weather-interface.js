74f26b55b9376770c74e5f8932d80c0e24f5172d
Gloria Friesen and Mark Fisher <mark@>
2017-04-17 14:43:29 -0700

    add .env file with API key

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,12 +1,20 @@
-var apiKey = "0978fa3dfd279e09a748a735d11c976a";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
   $("#weather-location").click(function(){
     var city = $("#location").val();
     $("#location").val("");
     $(".showWeather").text("The city you have chosen is " + city + ".");
-    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey, function(response){
-      $(".showWeather").text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
+
+    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey)
+      .then(function(response){
+        // console.log(response.responseJSON.message);
+        // // console.log(error.responseJSON.message);
+        $(".showWeather").text("The humidity in " + city + " is " + response.main.humidity + "%");
+      })
+
+      .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+      });
   });
 });
